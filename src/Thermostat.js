f27fbcbff1199df1fe4a25b3cb2b43e1ca192756
Rebecca Piper <piper.rebecca93@gmail.com>
2016-02-18 12:17:49 +0000

    Added colours depending on temperature. Refactored.

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,6 +3,23 @@
 function Thermostat() {
   this.temperature = 20;
   this.powerSM = true;
+  this.MIN_TEMP = 10;
+  this.MAX_TEMP_PSM_ON = 25;
+  this.MAX_TEMP_PSM_OFF = 32;
+};
+
+Thermostat.prototype.currentTemp = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.displayTemp = function() {
+  if(this.temperature < 18){
+    return "GREEN";
+  }
+  else if(this.temperature < 25) {
+    return "YELLOW";
+  };
+  return "RED";
 };
 
 Thermostat.prototype.upButton = function() {
@@ -24,14 +41,14 @@ Thermostat.prototype.changeMode = function() {
 };
 
 Thermostat.prototype._minTemp = function() {
-  return this.temperature === 10;
+  return this.temperature === this.MIN_TEMP;
 };
 
 Thermostat.prototype._maxTemp = function() {
   if(this.powerSM === true){
-    return this.temperature === 25;
+    return this.temperature === this.MAX_TEMP_PSM_ON;
   }
   else {
-    return this.temperature === 32;
+    return this.temperature === this.MAX_TEMP_PSM_OFF;
   };
 };
